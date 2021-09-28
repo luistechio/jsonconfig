37980d52fcb5f44032c59b185af855ab7d3f5193
luistechio <luistechio@gmail.com>
2021-09-28 13:24:14 -0300

    Update jsonutils.py

--- jsonconfig/jsonutils.py
+++ jsonconfig/jsonutils.py
@@ -5,7 +5,7 @@ import box
 
 def from_json_file(filename, **from_json_kwargs):
     try:
-        return box._from_json(filename=filename, **from_json_kwargs)
+        return box.box._from_json(filename=filename, **from_json_kwargs)
     except EnvironmentError:
         return {}
     except ValueError as e:
@@ -14,16 +14,16 @@ def from_json_file(filename, **from_json_kwargs):
 
 def to_json_file(data, filename, **to_json_kwargs):
     try:
-        return box._to_json(data, filename=filename, **to_json_kwargs)
+        return box.box._to_json(data, filename=filename, **to_json_kwargs)
     except EnvironmentError as e:
         raise FileError(e)
 
 
 def from_json(data, **from_json_kwargs):
     try:
-        return box._from_json(data, **from_json_kwargs)
+        return box.box._from_json(data, **from_json_kwargs)
     except (ValueError, TypeError) as e:
         raise JsonDecodeError(e)
 
 
-to_json = box._to_json
+to_json = box.box._to_json
